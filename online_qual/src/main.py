1592a55a0a88ee5f9932d5ab8e8eb329a2758f94
laujamie <36782341+laujamie@users.noreply.github.com>
2020-02-20 20:12:04 +0100

    Format code with yapf

--- online_qual/src/main.py
+++ online_qual/src/main.py
@@ -2,23 +2,16 @@ from sys import stdin
 
 if __name__ == "__main__":
 
-  books_libraries_days = input().split
-  book_scores = input().split
+    books_libraries_days = input().split
+    book_scores = input().split
 
-  libraries = []
+    libraries = []
 
-  class Library:
-    def __init__(self, library_data):
-      self.num_books = int(library_data[0])
-      self.num_signup_days = int(library_data[1])
-      self.book_ship_rate = int(library_data[2])
+    class Library:
+        def __init__(self, library_data):
+            self.num_books = int(library_data[0])
+            self.num_signup_days = int(library_data[1])
+            self.book_ship_rate = int(library_data[2])
 
-  for line in stdin:
-    libraries.append(Library(line))
-
-
-
-  
-    
-
-
+    for line in stdin:
+        libraries.append(Library(line))
