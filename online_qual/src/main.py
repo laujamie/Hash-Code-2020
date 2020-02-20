c031978623013230fe21dca5c3879d1cf28e72bf
laujamie <36782341+laujamie@users.noreply.github.com>
2020-02-20 20:13:35 +0100

    Move library class to be global

--- online_qual/src/main.py
+++ online_qual/src/main.py
@@ -1,17 +1,19 @@
 from sys import stdin
 
+
+class Library:
+    def __init__(self, library_data):
+        self.num_books = int(library_data[0])
+        self.num_signup_days = int(library_data[1])
+        self.book_ship_rate = int(library_data[2])
+
+
 if __name__ == "__main__":
 
     books_libraries_days = input().split
     book_scores = input().split
 
     libraries = []
-
-    class Library:
-        def __init__(self, library_data):
-            self.num_books = int(library_data[0])
-            self.num_signup_days = int(library_data[1])
-            self.book_ship_rate = int(library_data[2])
 
     for line in stdin:
         libraries.append(Library(line))
