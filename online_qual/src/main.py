56207335d29a82354845e17a33e1cd6a9d4b20ce
laujamie <36782341+laujamie@users.noreply.github.com>
2020-02-20 20:39:10 +0100

    Finish input handling

--- online_qual/src/main.py
+++ online_qual/src/main.py
@@ -2,18 +2,29 @@ from sys import stdin
 
 
 class Library:
-    def __init__(self, library_data):
+    def __init__(self, library_data, books):
         self.num_books = int(library_data[0])
         self.num_signup_days = int(library_data[1])
         self.book_ship_rate = int(library_data[2])
+        self.books = books
 
 
 if __name__ == "__main__":
 
-    books_libraries_days = input().split
-    book_scores = input().split
+    books_libraries_days = input().split()
+    book_scores = input().split()
+
+    print(books_libraries_days)
+    print(book_scores)
 
     libraries = []
 
-    for line in stdin:
-        libraries.append(Library(line))
+    while True:
+        try:
+            library_data = input().split()
+            library_books = input().split()
+            libraries.append(Library(library_data, library_books))
+        except EOFError:
+            break
+    
+    print(libraries)
